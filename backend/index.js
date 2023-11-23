4b952a4ae091b80bec1ccf771ca7ead4d58f202f
ayushi jain08 <ayushijain0807@gmail.com>
2023-11-23 21:18:43 +0530

    add

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,12 @@ const app = express();
 import connectDB from "./Config/db.js";
 import fileUpload from "express-fileupload";
 import cors from "cors";
+import path from "path";
 import user from "./Routes/User.js";
 import review from "./Routes/Review.js";
 
 connectDB();
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
@@ -31,6 +33,11 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
+});
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", " build", "index.html"));
 });
 const PORT = process.env.PORT || 7000;
 
